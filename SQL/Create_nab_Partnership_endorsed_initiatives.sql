30aa0fa5bd00e710a3af1cd80f416a5f3718deee
Kevin Wu <kevin.x.wu@nab.com.au>
2018-09-04 18:05:59 +1000

    separated partnerships table into 2, added system_creation_date column to sort df display, hide team recommendation for type = execution

---
+++ SQL/Create_nab_Partnership_endorsed_initiatives.sql
@@ -0,0 +1,15 @@
+CREATE TABLE nab_Partnership_endorsed_initiatives
+(
+initiative_name VARCHAR(150),
+initiative_short_description  VARCHAR(250),
+initiative_category VARCHAR(100),
+creation_date DATE,
+type  VARCHAR(100),
+stakeholder VARCHAR(50),
+team_recommendation VARCHAR(5),
+endorsement VARCHAR(1),
+endorsement_date  Date,
+endorsement_body  VARCHAR(200),
+system_creation_date Timestamp
+) ;
+
